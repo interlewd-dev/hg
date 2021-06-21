1477f77d35b81df67853f4d8cb3e066b389480ab
Interlewd dev <interlewdd@gmail.com>
2021-06-21 03:12:57 -0400

    Add initial state to player reducer

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,4 +1,9 @@
+const initialState = {};
+
 const player = (state, action) => {
+  if (state === undefined)
+    return initialState;
+
   switch (action.type) {
   case 'PLAYER/SET_PRONOUNS':
     return Object.assign({}, state, action.pronouns);
